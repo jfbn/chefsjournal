bacd0ba83c812988bfa4b49046b04a8ef77bf439
jfbn <johan.bagger@hotmail.com>
2020-06-19 16:08:55 +0200

    allows signup on webpage

--- connection/mongodb_connection.js
+++ connection/mongodb_connection.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 const { local, remote } = require('../config/dbconfig.json');
 
+const useLocal = false;
 
+if(useLocal) {
+  mongoose.connect(`mongodb://${local.DB_ADRESS}:${local.DB_PORT}/${local.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
+    if(err){
+      console.log("error: " + err);
+    }      
+    }).then( () => {
+      console.log("connected to local db")
+    });
+} else {
+  mongoose.connect(remote.DB_ADRESS, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
+    if(err){
+      console.log(err);
+    }      
+    }).then( () => {
+      console.log("connected to remote db")
+    });
+}
 
-mongoose.connect(`mongodb://${local.DB_ADRESS}:${local.DB_PORT}/${local.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
-  if(err){
-    console.log(err);
-  }      
-  }).then( () => {
-    console.log("connected to db")
-  });
