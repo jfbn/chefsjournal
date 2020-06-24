42012424ecd74eaa335644b0af0e3054c93a9532
jfbn <johan.bagger@hotmail.com>
2020-06-24 18:35:23 +0200

    potential fix v2

--- connection/mongodb_connection.js
+++ connection/mongodb_connection.js
@@ -1,20 +1,29 @@
 const mongoose = require('mongoose');
 const { local, remote } = require('../config/dbconfig.json');
-const useLocal = process.env.USELOCAL;
+const USELOCAL = process.env.USELOCAL;
+console.log("HERE I AM, THE PROCESS.ENV.USELOCAL IS SET TO: "+ USELOCAL);
 
-if(useLocal) {
+if(USELOCAL) {
   mongoose.connect(`mongodb://${local.DB_ADRESS}:${local.DB_PORT}/${local.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
-    if(err){
-      console.log("error: " + err);
-    }      
+      if(err){
+        console.log("error: " + err);
+      }      
+    }).catch(err => {
+      console.log("there was an error connecting to local db");
+      console.log("the error:");
+      console.log(err);
     }).then( () => {
       console.log("connected to local db")
     });
 } else {
   mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true}, err => {
-    if(err){
-      console.log(err);
-    }      
+      if(err){
+        console.log(err);
+      }
+    }).catch(err => {
+      console.log("there was an error connecting to local db");
+      console.log("the error:");
+      console.log(err);      
     }).then( () => {
       console.log("connected to remote db")
     });
